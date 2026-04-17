2d1f5f2eb41fee292c05a5f9e8cebd91510e054c
Atonu Roy Chowdhury <166700173+atonu-a@users.noreply.github.com>
2026-04-18 00:32:36 +0600

    Update settings.py

--- blogging/settings.py
+++ blogging/settings.py
@@ -30,16 +30,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = 'blogging.urls'
+ROOT_URLCONF  = 'blogging.urls'
 
 TEMPLATES = [
     {
