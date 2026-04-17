4e612b94a1f192002416860ca8834c7618bb7cff
Atonu Roy Chowdhury <166700173+atonu-a@users.noreply.github.com>
2026-04-17 23:26:05 +0600

    Replace createsuperuser command with shell script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-python manage.py createsuperuser --no-input --username AtonuRoy --email atonu734432gmail.com || true
+python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User = get_user_model(); u, created = User.objects.get_or_create(username='AtonuRoy'); u.set_password(os.environ.get('DJANGO_SUPERUSER_PASSWORD')); u.is_superuser=True; u.is_staff=True; u.save()"
 
