c858764faf8074d6a31cb683be2acb217229db4e
atonu-a <166700173+atonu-a@users.noreply.github.com>
2026-04-18 17:35:23 +0600

    Default user profile picture added

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.templatetags.static import static
 
 class Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,7 +12,7 @@ class Profile (models.Model):
     def get_profile_pic(self):
         if self.profile_pic:
             return self.profile_pic.url
-        return '/static/images/other/author-default.png'
+        return static('images/other/author-default.png')
     
     def __str__(self):
         return self.user.username
