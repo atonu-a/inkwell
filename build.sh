c4601906f7c633e585e9727510d699bb7cdfeadf
atonu-a <166700173+atonu-a@users.noreply.github.com>
2026-04-18 17:19:07 +0600

    new superuser creation

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User = get_user_model(); u, created = User.objects.get_or_create(username='AtonuRoy'); u.set_password(os.environ.get('DJANGO_SUPERUSER_PASSWORD')); u.is_superuser=True; u.is_staff=True; u.save()"
+python manage.py shell -c "from django.contrib.auth import get_user_model; import os; User = get_user_model(); u, created = User.objects.get_or_create(username='AtonuRoyCh'); u.set_password(os.environ.get('DJANGO_SUPERUSER_PASSWORD')); u.is_superuser=True; u.is_staff=True; u.save()"
+
 
