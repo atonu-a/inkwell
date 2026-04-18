72420c6c81e8b4f3d17d2f743ce7768cdcd51b44
Atonu Roy Chowdhury <166700173+atonu-a@users.noreply.github.com>
2026-04-18 11:16:41 +0600

    Configure Cloudinary storage settings
    Added Cloudinary storage configuration for media files.

--- blogging/settings.py
+++ blogging/settings.py
@@ -94,5 +94,11 @@ cloudinary.config(
     api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
 )
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
