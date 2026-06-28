5e2b79db7291f35a9e9a6f352c6a982a36151d2a
Atonu <susnigdho1234@gmail.com>
2026-06-28 19:50:45 +0600

    Remove unused Font Awesome webfont files and update back to top link in base template

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,10 @@
 
+
+window.addEventListener("load", () => {
+  const rtrnToTop = document.querySelector("#return-to-top");
+  rtrnToTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+});
