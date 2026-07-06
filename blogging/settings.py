8ee7521853267f0e3362b7cedd3cd7a3c603ea48
Atonu Roy Chowdhury <166700173+atonu-a@users.noreply.github.com>
2026-07-07 00:51:28 +0600

    Disable TLS and enable SSL for email configuration

--- blogging/settings.py
+++ blogging/settings.py
@@ -74,7 +74,8 @@ MIDDLEWARE = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
